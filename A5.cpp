d6603074a5be20f67c4d23b2f2a6b535c37bbdf9
Moemen Khchim madi <madokakhchimadi@gmail.com>
2024-03-15 14:10:45 +0100

    New solved problem

---
+++ A5.cpp
@@ -0,0 +1,14 @@
+/*Problem link :
+https://codeforces.com/problemset/problem/50/A
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  int M,N;
+  cin>>M>>N;
+  cout<<(M*N)/2<<endl;
+  return 0;
+}
+//accepted
