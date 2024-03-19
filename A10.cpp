0e045e9e47f8123ba55a6a29da0b3055576eab80
Moemen Khchim madi <madokakhchimadi@gmail.com>
2024-03-19 16:20:21 +0100

    New solved problem

---
+++ A10.cpp
@@ -0,0 +1,35 @@
+/*Problem link :
+https://codeforces.com/problemset/problem/266/B
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int n,t;
+    cin>>n>>t;
+    string s;
+    cin>>s;
+    for(int i=0;i<t;i++)
+    {
+        int j=0;
+        while(j<n)
+        {
+            if(s[j]=='B' && s[j+1]=='G')
+            {
+               s[j]='G'; 
+               s[j+1]='B' ;
+               j=j+2;
+            }
+            else
+            {
+                j++;
+            }
+        }
+        j=0;
+    } 
+    std::cout<<s<<endl;     
+    return 0;
+}
+//accepted
