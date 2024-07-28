62f8ac194c7aea3a3bec362f25f3782efdef7b44
Moemen Khchim madi <madokakhchimadi@gmail.com>
2024-07-28 01:27:07 +0100

    Solved new problem

---
+++ A43.cpp
@@ -0,0 +1,30 @@
+/*Problem link :
+https://codeforces.com/problemset/problem/118/A
+*/
+#include <bits/stdc++.h>
+#define all(l) (l).begin(), (l).end()
+#define mini(v) (*min_element(all(v)))
+#define maxi(v) (*max_element(all(v)))
+#define ll unsigned long long
+#define sll signed long long
+#define vl vector<long>
+#define vll vector<long long>
+#define p push_back
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    ll t;
+    cin >> t;
+    for (ll i = 0; i < t; i++)
+    {
+        ll n;
+        cin>>n;
+        cout<<n-1<<endl;
+        
+    }
+
+    return 0;
+}
+// accepted
